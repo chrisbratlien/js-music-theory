1116eaab57d2161adcc993f3d155bc805c651806
Chris Bratlien <chrisbratlien@gmail.com>
2009-04-27 10:49:41 -0500

    initial commit

---
+++ js-music-theory.js
@@ -0,0 +1,21 @@
+/*
+if (typeof Object.beget !== 'function') {
+     Object.beget = function (o) {
+         var F = function () {};
+         F.prototype = o;
+         return new F();
+     };
+}
+*/
+
+function RootNoteWithIntervals(rn, intervals = Array()) {
+
+	var result = {
+		rootNote: rn,
+		intervals: i
+	};
+		
+  return result;
+}
+
+
