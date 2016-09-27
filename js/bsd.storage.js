a0a95ddfc218623afdc5b0b1ec6bb349478a23fd
Chris Bratlien <chrisbratlien@gmail.com>
2016-09-26 22:26:15 -0500

    check error case

--- js/bsd.storage.js
+++ js/bsd.storage.js
@@ -1,19 +1,28 @@
-if (typeof BSD ==  "undefined") { var BSD = {}; }
-BSD.Storage = function(prefix) {
-  if (!prefix) { throw('configuration error: set BSD.Storage needs prefix'); 
+if (!BSD) {
+  var BSD = {}; 
+}
+
+BSD.Storage = function (prefix) {
+  if (!prefix) { 
+    throw('configuration error: set BSD.Storage needs prefix'); 
     return false;
-  };
+  }
+
   var self = {};
   self.setItem = function(k,v) {
     return localStorage.setItem(prefix + k,v);
   };
+
   self.getItem = function(k,success,error) {
     var result = localStorage.getItem(prefix + k);
     if (success && result) { 
       success(result); 
       return result;
     }
-    error(result);
+
+    if (error) {
+      error(result);     
+    }
     return result;
   };
   
