2d6264d6768c01736c2cbcc4d7941cbbd377c838
Chris Bratlien <chrisbratlien@gmail.com>
2017-09-23 16:20:21 -0500

    mixobluesian

--- js/js-music-theory.js
+++ js/js-music-theory.js
@@ -213,6 +213,7 @@ JSMT.scaleMap = {
 
     'dominant' : { name: 'dominant', intervals: [0,2,4,5,7,9,10] },
     'mixolydian' : { name: 'mixolydian', intervals: [0,2,4,5,7,9,10] },
+    'mixobluesian' : { name: 'mixobluesian', intervals: [0,2,3,4,5,6,7,9,10] },
 
 
     'locrian' : { name: 'locrian', intervals: [0,1,3,5,6,8,10] },
