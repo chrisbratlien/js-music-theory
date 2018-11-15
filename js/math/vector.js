ebfbd79f742fcbe5141bd9e8ff678cbeb0710f5e
Chris Bratlien <chrisbratlien@gmail.com>
2018-11-15 00:19:53 -0600

    vector experiments

--- js/math/vector.js
+++ js/math/vector.js
@@ -1,5 +1,24 @@
 class Vector extends Array {
+    static debug_sum(...them) {
+        //var result = new Vector(Array(them.length).fill(0));
+        console.log('them?',them);
+        var result = them.reduce( (accum,current) => { 
+            console.log('accum?',accum);
+            return accum.add(current)
+
+
+        });
+        return result;
+    }
+
+    static sum(...vectors) {
+        return vectors.reduce( (accum,current) => accum.add(current) );
+    }
+
     scale(r) {
         return this.map(o => r*o);
+    }
+    add(v) {
+        return this.map((o,i) => o + v[i])
     }
 }
