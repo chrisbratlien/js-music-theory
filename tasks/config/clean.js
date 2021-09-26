214517d1979d05fd64d66c2ad18e4780656573ca
Chris Bratlien <c-bratlien@tti.tamu.edu>
2021-09-25 19:40:55 -0500

    remove grunt and old npm/node_modules

--- tasks/config/clean.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  pre: [ 'styles/build', 'javascript/build' ],
-  post: [ 'styles/build/app.*', 'javascript/build/app.*' ]
-};
--- tasks/register/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (grunt) {
-  grunt.registerTask('dev', [
-    'clean:pre',
-    'concat:css',
-    'concat:js',
-    'string-replace:dev'
-  ]);
-};
