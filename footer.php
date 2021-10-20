4e01da99ff804b17cc4ad7eaaa2f109efe635bc5
Chris Bratlien <chrisbratlien@gmail.com>
2021-10-19 20:59:51 -0500

    better BSD.baseURL for when installed in a subdirectory of the host

--- footer.php
+++ footer.php
@@ -3,9 +3,12 @@
     if (typeof BSD == "undefined") {
       var BSD = {};
     }
-    
-    BSD.baseURL = window.location.origin;
-</script>
+    //this works great when installed at the apex, but not at a subdirectory
+    //BSD.baseURL = window.location.origin;
+
+    //should work better for a subdirectory install.
+    BSD.baseURL = `<?php home_url(); ?>`;
+  </script>
 
   </script>
 
@@ -20,34 +23,34 @@
   <script src="lib/require.js"></script>
   -->
 
-  <script src="<?php  home_url();  ?>/lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?php home_url();  ?>/lib/bootstrap/js/bootstrap.min.js"></script>
 
-  <script src="<?php  home_url();  ?>/js/array.js"></script>
-  <script src="<?php  home_url();  ?>/js/color.js"></script>
-  <script src="<?php  home_url();  ?>/js/bsd.pubsub.js"></script>
+  <script src="<?php home_url();  ?>/js/array.js"></script>
+  <script src="<?php home_url();  ?>/js/color.js"></script>
+  <script src="<?php home_url();  ?>/js/bsd.pubsub.js"></script>
 
 
 
-  <script src="<?php  home_url();  ?>/js/js-music-theory.js"></script>
-  <script src="<?php  home_url();  ?>/js/bsd.widgets.baseplayer.js"></script>
-  <script src="<?php  home_url();  ?>/js/bsd.widgets.stringoscillator.js"></script>
-  <script src="<?php  home_url();  ?>/js/bsd.widgets.guitarplayer.js"></script>
+  <script src="<?php home_url();  ?>/js/js-music-theory.js"></script>
+  <script src="<?php home_url();  ?>/js/bsd.widgets.baseplayer.js"></script>
+  <script src="<?php home_url();  ?>/js/bsd.widgets.stringoscillator.js"></script>
+  <script src="<?php home_url();  ?>/js/bsd.widgets.guitarplayer.js"></script>
 
 
 
   <!-- may try out some variations on eachify -->
-  <script src="<?php  home_url();  ?>/js/eachify.js"></script>
-  <script src="<?php  home_url();  ?>/js/draggy.js"></script>
-  <script src="<?php  home_url();  ?>/js/sticky-note.js"></script>
+  <script src="<?php home_url();  ?>/js/eachify.js"></script>
+  <script src="<?php home_url();  ?>/js/draggy.js"></script>
+  <script src="<?php home_url();  ?>/js/sticky-note.js"></script>
 
   <!--<script src="lib/JavaScript-ID3-Reader/dist/id3-minimized.js"></script>-->
   <!-- <script src="lib/Kali/build/kali.js"></script>-->
 
-  <script src="<?php  home_url();  ?>/js/bsd.storage.js"></script>
-  <script src="<?php  home_url();  ?>/js/bsd.remotestorage.js"></script>
-  <script src="<?php  home_url();  ?>/js/bsd.widgets.procrastinator.js"></script>
-  <script src="<?php  home_url();  ?>/js/main.js" type="module" onload="onAppLoad()"></script>
-  <script src="<?php  home_url();  ?>/js/bootup.js?v=22"></script>
+  <script src="<?php home_url();  ?>/js/bsd.storage.js"></script>
+  <script src="<?php home_url();  ?>/js/bsd.remotestorage.js"></script>
+  <script src="<?php home_url();  ?>/js/bsd.widgets.procrastinator.js"></script>
+  <script src="<?php home_url();  ?>/js/main.js" type="module" onload="onAppLoad()"></script>
+  <script src="<?php home_url();  ?>/js/bootup.js?v=22"></script>
 
 
   <?php wp_footer(); ?>
